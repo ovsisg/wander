80914bb29c53858410a58d36dcde72fbf0fc1686
ovsisg <ovsisg@gmail.com>
2025-10-25 22:47:43 +0100

    Added map initialisation

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+class App {
+  #map;
+  #mapZoomLevel = 13;
+
+  constructor() {
+    this._getPosition();
+  }
+
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('Unable to get your location');
+        }
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+  }
+}
+
+const app = new App();
