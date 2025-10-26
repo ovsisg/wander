4af77150d7d642bd290a993f12f362a4770106bb
ovsisg <ovsisg@gmail.com>
2025-10-26 12:48:05 +0000

    Displayed form on map click

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 'use strict';
 
+const form = document.querySelector('.form');
+const inputType = document.querySelector('.form__input--type');
+const inputRating = document.querySelector('.form__input--rating');
+const inputPlannedDate = document.querySelector('.form__input--planned-date');
+
 class App {
   #map;
+  #mapEvent;
   #mapZoomLevel = 13;
 
   constructor() {
     this._getPosition();
+
+    form.addEventListener('submit', this._newPlace.bind(this));
+    inputType.addEventListener('change', this._toggleOptionalFields);
   }
 
   _getPosition() {
@@ -21,7 +30,6 @@ class App {
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-
     const coords = [latitude, longitude];
 
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
@@ -30,6 +38,24 @@ class App {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+  }
+
+  _toggleOptionalFields() {
+    inputPlannedDate
+      .closest('.form__row')
+      .classList.toggle('form__row--hidden');
+    inputRating.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newPlace(e) {
+    e.preventDefault();
   }
 }
 
